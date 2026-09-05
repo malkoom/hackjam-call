010e8eded0a869d267f70fefb28672fe5bbaf7ff
S*ck my Wand <sck.my.wand@gmail.com>
2026-09-05 09:48:46 +0200

    Juego Caracoles

--- Assets/Scripts/Minigames/Minigame_Snail.cs
+++ Assets/Scripts/Minigames/Minigame_Snail.cs
@@ -1,5 +1,6 @@
 
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -22,8 +23,18 @@ public class Minigame_Snail : AMiniGame
 
     private bool ActiveGame = true;
 
-    //[Header("Keys")]
-    //private Keyboard[] player1KeySequence = { Keyboard.current.upArrowKey, Keyboard.current.rightArrowKey }
+    [Header("Keys")]
+    private Key[] player1KeySequence = { Key.W, Key.D, Key.S, Key.A };
+    private int playerKeyIndex = 0;
+    private Key[] player2KeySequence = {Key.UpArrow,Key.RightArrow, Key.DownArrow, Key.LeftArrow};
+    private int player2KeyIndex = 0;
+
+    [Header("UI")]
+    public GameObject key;
+    public GameObject key2;
+
+    [SerializeField] public Dictionary<Key, Sprite> p1KeysDictionary = new Dictionary<Key, Sprite>();
+    [SerializeField] public Dictionary<Key, Sprite> p2KeysDictionary = new Dictionary<Key, Sprite>();
 
 
     void Start()
@@ -42,20 +53,37 @@ public class Minigame_Snail : AMiniGame
 
     void Update()
     {
-        
+
+
         if (!ActiveGame)
         {
             return;
         }
 
-        if(Keyboard.current.wKey.wasPressedThisFrame)
+        if (Keyboard.current[player1KeySequence[playerKeyIndex]].wasPressedThisFrame)
         {
             Snail.transform.position += new Vector3(0.3f, 0, 0);
+            playerKeyIndex++;
+
+            if (playerKeyIndex >= player1KeySequence.Length)
+            {
+                playerKeyIndex = 0;
+            }
+
+            key.GetComponent<SpriteRenderer>().sprite = p1KeysDictionary[player1KeySequence[playerKeyIndex]];
         }
 
-        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        if (Keyboard.current[player2KeySequence[player2KeyIndex]].wasPressedThisFrame)
         {
             Snail2.transform.position += new Vector3(0.3f, 0, 0);
+            player2KeyIndex++;
+
+            if (player2KeyIndex >= player2KeySequence.Length)
+            {
+                player2KeyIndex = 0;
+            }
+
+            key2.GetComponent<SpriteRenderer>().sprite = p2KeysDictionary[player2KeySequence[player2KeyIndex]];
         }
 
         if (Snail.transform.position.x >= FinalPosX || Snail2.transform.position.x >= FinalPosX)
